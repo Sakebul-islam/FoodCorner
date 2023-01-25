100d62eeda4048cac93075751d5b412a2fe417b7
Sakebul-islam <mdsakebul66@gmail.com>
2023-01-25 12:40:51 +0600

    On Cart click Modal component open and on overlay and Model component close button click Model component will be disappear functionnaliti is added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 
 import './App.css';
 import Header from './components/Layout/Header/Header';
@@ -6,10 +6,19 @@ import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
 
 function App() {
+  const [cartlsShown, setCartlsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartlsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartlsShown(false);
+  };
   return (
     <Fragment>
-      <Cart/>
-      <Header />
+      {cartlsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
