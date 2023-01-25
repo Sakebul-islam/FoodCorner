0d7bdc4a49f58a181eb79e146935e7e781354630
Sakebul-islam <mdsakebul66@gmail.com>
2023-01-25 16:16:03 +0600

    Project Done

--- src/components/Layout/Header/HeaderCardButton.jsx
+++ src/components/Layout/Header/HeaderCardButton.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import CartIcon from '../../Cart/CartIcon/CartIcon';
 import CartContext from '../../../context/cart-context';
@@ -6,13 +6,33 @@ import CartContext from '../../../context/cart-context';
 import classes from './HeaderCardButton.module.css';
 
 const HeaderCardButton = props => {
+  const [btnIsHihlighted, setBtnIsHihlighted] = useState(false);
   const cartCtx = useContext(CartContext);
+  
+  const { items } = cartCtx;
 
-  const numberOfCartItem = cartCtx.items.reduce((currentNumber, item) => {
+  const numberOfCartItem = items.reduce((currentNumber, item) => {
     return currentNumber + item.amount;
   }, 0);
+
+
+  const btnClasses = `${classes.button} ${btnIsHihlighted ? classes.bump : ''}`;
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setBtnIsHihlighted(true);
+    const timer = setTimeout(()=>{
+    setBtnIsHihlighted(false);
+    },300)
+
+    return ()=>{
+      clearTimeout(timer)
+    }
+  }, [items]);
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
